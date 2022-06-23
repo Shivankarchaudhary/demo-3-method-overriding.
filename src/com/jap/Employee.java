a8432e148903d7b67c11231aa6f4bf84dadc964b
bhakti.rathod <bhakti.rathod@stackroute.in>
2022-06-23 15:29:47 +0530

    Initial commit

---
+++ src/com/jap/Employee.java
@@ -0,0 +1,22 @@
+package com.jap;
+
+public class Employee {
+
+    private String empName;
+    private float salary;
+    private float bonus;
+
+
+    public Employee(String empName, float salary, float bonus) {
+        this.empName = empName;
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+
+    public float calculateSalary(){
+        float totalSalary = salary + bonus;
+        return totalSalary;
+
+    }
+
+}
